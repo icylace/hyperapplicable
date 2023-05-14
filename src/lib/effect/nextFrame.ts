2b87ac8e3c16663610175ff7115935697e86cc9a
Ron Martinez <ramartin.net@gmail.com>
2023-05-14 12:48:49 -0400

    wip

--- src/lib/effect/nextFrame.ts
+++ src/lib/effect/nextFrame.ts
@@ -1,12 +1,16 @@
 import { Dispatch, Effect } from "hyperapp"
 
-export const nextFrame = <S>(f: Function, ...args: any[]): Effect<S> =>
-  [runNextFrame, { f, args }]
+export const nextFrame = <S>(
+  f: (..._: any[]) => Record<string, any> | null,
+  ...args: any[]
+): Effect<S> => {
+  return [runNextFrame, { f, args }]
+}
 
 const runNextFrame = <S>(dispatch: Dispatch<S>, props: any): void => {
   window.requestAnimationFrame(async (): Promise<void> => {
     const newState = await props.f(...props.args)
     if (newState instanceof Error) throw newState
-    dispatch((state) => newState == null ? state : newState)
+    dispatch((state) => newState == null ? state : { ...state, ...newState })
   })
 }
